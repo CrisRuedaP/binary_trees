7979e3c5613441965f7806371a2ecd4c68811fcf
CrisRuedaP <crisruedap@gmail.com>
2020-07-24 20:08:49 +0000

    Fixed output

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,8 +7,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left = 0;
-	size_t right = 0;
+	size_t left, right;
 
 	if (tree == NULL)
 		return (0);
@@ -17,7 +16,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 	if (left > right)
 		return (left + 1);
 	else
